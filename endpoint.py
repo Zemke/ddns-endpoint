4caa396cee9d2eea5f21cdc1777aa4c391281fe0
Zemke <florian@zemke.io>
2020-04-01 11:14:09 +0200

    Parse query params

--- endpoint.py
+++ endpoint.py
@@ -1,4 +1,5 @@
 import os
+import re
 import sys
 
 import web
@@ -6,20 +7,32 @@ import web
 urls = ('/.*', 'index')
 
 
-def do_the_boogy():
+def do_the_boogy(pp):
+  #  todo pass pp
   os.system(" ".join(sys.argv[2:]))
+
+
+p_compile = re.compile("^.+=()$")
+
+
+def parse_params(params):
+  addr = re.compile("addr=(.+?)(?:&|\?|$)").search(params).group(1)
+  secret = re.compile("secret=(.+?)(?:&|\?|$)").search(params).group(1)
+  domain = re.compile("domain=(.+?)(?:&|\?|$)").search(params).group(1)
+  return {"addr": addr, "secret": secret, "domain": domain}
 
 
 class index:
   def GET(self):
-    do_the_boogy()
+    do_the_boogy(parse_params(web.ctx.query))
     return "yeah, you're here"
     pass
 
-  def POST(self):
-    do_the_boogy()
-    return "yeah, you're here POST"
-    pass
+
+def POST(self):
+  do_the_boogy(parse_params(web.ctx.query))
+  return "yeah, you're here POST"
+  pass
 
 
 if __name__ == "__main__":
