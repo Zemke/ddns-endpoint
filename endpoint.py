de8414907bb0e4219a532cdbc2d12562be58cadd
Zemke <florian@zemke.io>
2020-04-05 14:35:16 +0200

    Fixes all over the place

--- endpoint.py
+++ endpoint.py
@@ -10,7 +10,7 @@ urls = ('/.*', 'index')
 def do_the_boogy(pp):
   #  todo pass pp
   os.system(" ".join(sys.argv[2:]))
-
+  
 
 p_compile = re.compile("^.+=()$")
 
@@ -20,21 +20,22 @@ def parse_params(params):
   secret = re.compile("secret=(.+?)(?:&|\?|$)").search(params).group(1)
   domain = re.compile("domain=(.+?)(?:&|\?|$)").search(params).group(1)
   return {"addr": addr, "secret": secret, "domain": domain}
-
+  
 
 class index:
   def GET(self):
-    do_the_boogy(parse_params(web.ctx.query))
-    return "yeah, you're here"
-    pass
+    print("query", web.ctx.query)
+    print("boogy", do_the_boogy(parse_params(web.ctx.query)))
+    return "success"
 
-
-def POST(self):
-  do_the_boogy(parse_params(web.ctx.query))
-  return "yeah, you're here POST"
-  pass
+  def POST(self):
+    print("query", web.ctx.query)
+    print("data", web.data())
+    print("boogy", do_the_boogy(parse_params(web.ctx.query)))
+    return "success"
 
 
 if __name__ == "__main__":
   app = web.application(urls, globals())
   app.run()
+
